b800aefa2c4cee8f8fd1115219b8e2a2e36e91d9
AuriRex <xaurirexx@gmail.com>
2021-12-01 20:06:27 +0100

    QOL: R4 hacking minigame & R# in header

--- TheArchive.IL2CPP/HarmonyPatches/Patches/QualityOfLifePatches.cs
+++ TheArchive.IL2CPP/HarmonyPatches/Patches/QualityOfLifePatches.cs
@@ -1,18 +1,78 @@
-﻿using Player;
+﻿using AK;
+using LevelGeneration;
+using Player;
 using System;
+using System.Reflection;
 using TheArchive.Core.Core;
 using TheArchive.Utilities;
 using UnityEngine;
+using static HackingTool;
 using static TheArchive.Core.ArchivePatcher;
+using static TheArchive.Utilities.Utils;
 
 namespace TheArchive.HarmonyPatches.Patches
 {
     [BindPatchToSetting(nameof(ArchiveSettings.EnableQualityOfLifeImprovements), "QOL")]
     public class QualityOfLifePatches
     {
+        // add R4 / R5 to the beginning of the header text
+        [ArchivePatch(typeof(PlayerGuiLayer), "UpdateObjectiveHeader")]
+        internal static class PlayerGuiLayer_UpdateObjectiveHeaderPatch
+        {
+            public static void Prefix(ref string header)
+            {
+                header = $"R{ArchiveMod.CurrentRundown.GetIntValue()}{header}";
+            }
+        }
 
-        // TODO: R4 Hacking minigame fix
-        // TODO: R4 + R5 Rundown number in top left corner
+        // Change hacking minigame to be more in line with newest version of the game -> minigame finishes and hack disappears instantly
+        [ArchivePatch(typeof(HackingTool), "UpdateHackSequence", RundownFlags.RundownFour)]
+        internal static class HackingTool_UpdateHackSequencePatch
+        {
+            private static MethodInfo HackingTool_ClearScreen = typeof(HackingTool).GetMethod("ClearScreen", AnyBindingFlags);
+            private static MethodInfo HackingTool_OnStopHacking = typeof(HackingTool).GetMethod("OnStopHacking", AnyBindingFlags);
+
+            public static bool Prefix(ref HackingTool __instance)
+            {
+                try
+                {
+
+                    switch (__instance.m_state)
+                    {
+                        case HackSequenceState.DoneWait:
+                            __instance.m_activeMinigame.EndGame();
+                            __instance.m_holoSourceGFX.SetActive(value: false);
+                            __instance.Sound.Post(EVENTS.BUTTONGENERICBLIPTHREE);
+                            __instance.m_stateTimer = 1f;
+                            if (__instance.m_currentHackable != null)
+                            {
+                                LG_LevelInteractionManager.WantToSetHackableStatus(__instance.m_currentHackable, eHackableStatus.Success, __instance.Owner);
+                            }
+                            __instance.m_state = HackSequenceState.Done;
+                            return false;
+                        case HackSequenceState.Done:
+                            if (__instance.m_stateTimer < 0f)
+                            {
+                                HackingTool_ClearScreen.Invoke(__instance, null);
+                                HackingTool_OnStopHacking.Invoke(__instance, null);
+                                __instance.Sound.Post(EVENTS.BUTTONGENERICSEQUENCEFINISHED);
+                                __instance.m_state = HackSequenceState.Idle;
+                            }
+                            else
+                            {
+                                __instance.m_stateTimer -= Clock.Delta;
+                            }
+                            return false;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MelonLoader.MelonLogger.Msg(ConsoleColor.Red, $"An error occured: {ex.Message}");
+                    MelonLoader.MelonLogger.Msg(ConsoleColor.Red, $"{ex.StackTrace}");
+                }
+                return true;
+            }
+        }
 
         // prioritize resources in ping raycasts
         [ArchivePatch(typeof(PlayerAgent), nameof(PlayerAgent.TriggerMarkerPing))]
