8b9c45e97365c74af61ab854334b21be066e1635
AuriRex <xaurirexx@gmail.com>
2022-02-12 23:35:01 +0100

    Loadout Randomizer for R1 - R3

--- TheArchive.MONO/Utilities/MonoUtils.cs
+++ TheArchive.MONO/Utilities/MonoUtils.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using System.Collections;
+using System.Reflection;
 using UnityEngine;
 
 namespace TheArchive.Utilities
 {
-    public class MonoUtils
+    public static class MonoUtils
     {
 
         // https://gamedev.stackexchange.com/a/183962
@@ -30,10 +31,10 @@ namespace TheArchive.Utilities
             yield break;
         }
 
-        public static void CallEvent<TOn>(string eventFieldName)
+        public static void CallEvent<TOn>(string eventFieldName, object instance = null)
         {
             var eventInfo = typeof(TOn).GetType().GetEvent(eventFieldName, HarmonyLib.AccessTools.all);
-            var eventDelegate = (MulticastDelegate) typeof(TOn).GetField(eventFieldName, HarmonyLib.AccessTools.all).GetValue(null);
+            var eventDelegate = (MulticastDelegate) typeof(TOn).GetField(eventFieldName, HarmonyLib.AccessTools.all).GetValue(instance);
             if (eventDelegate != null)
             {
                 foreach (var handler in eventDelegate.GetInvocationList())
@@ -44,5 +45,82 @@ namespace TheArchive.Utilities
             }
         }
 
+        public static void RemoveAllEventHandlers<TOn>(string eventFieldName, object instance = null)
+        {
+            var eventFieldInfo = typeof(TOn).GetField(eventFieldName, HarmonyLib.AccessTools.all);
+
+            eventFieldInfo?.SetValue(instance, null);
+        }
+
+        public static T PickRandom<T>(this T[] array)
+        {
+            return (T) array.GetValue(UnityEngine.Random.Range(0, array.Length - 1));
+        }
+
+        /// <summary>
+        /// Run <paramref name="func"/> on every first child of <paramref name="trans"/>
+        /// </summary>
+        /// <param name="trans"></param>
+        /// <param name="func"></param>
+        public static void ForEachFirstChildDo(this Transform trans, Action<Transform> func) => trans.ForEachChildDo(func, recursive: false);
+
+        /// <summary>
+        /// Run <paramref name="func"/> on every child of <paramref name="trans"/>
+        /// </summary>
+        /// <param name="trans"></param>
+        /// <param name="func"></param>
+        /// <param name="recursive">If the children of every child should be included</param>
+        public static void ForEachChildDo(this Transform trans, Action<Transform> func, bool recursive = true)
+        {
+            for (int i = 0; i < trans.childCount; i++)
+            {
+                var child = trans.GetChild(i);
+                func?.Invoke(child);
+                if (recursive)
+                {
+                    ForEachChildDo(child, func, recursive);
+                }
+            }
+        }
+
+        public static Transform GetChildWithExactName(this Transform trans, string name)
+        {
+            for (int i = 0; i < trans.childCount; i++)
+            {
+                var child = trans.GetChild(i);
+                if (child.name == name) return child;
+            }
+            return null;
+        }
+
+        public static T GetComponentOnSelfOrParents<T>(this Transform trans) where T : Component
+        {
+            if (trans == null) return null;
+
+            T component = trans.GetComponent<T>();
+
+            if (component != null)
+            {
+                return component;
+            }
+
+            return trans?.GetComponentOnParents<T>();
+        }
+
+        public static T GetComponentOnParents<T>(this Transform trans) where T : Component
+        {
+            if (trans == null) return null;
+
+            var parent = trans.parent;
+
+            T component = parent.GetComponent<T>();
+
+            if (component != null)
+            {
+                return component;
+            }
+
+            return parent.parent?.GetComponentOnParents<T>();
+        }
     }
 }
