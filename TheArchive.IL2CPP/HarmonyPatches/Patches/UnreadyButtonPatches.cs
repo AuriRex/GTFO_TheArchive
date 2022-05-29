a9a2dd5fa1d86959d46b8187837bf3fc602feb7a
AuriRex <xaurirexx@gmail.com>
2022-05-29 22:08:39 +0200

    Unready button for R1-R5

---
+++ TheArchive.IL2CPP/HarmonyPatches/Patches/UnreadyButtonPatches.cs
@@ -0,0 +1,39 @@
+﻿using CellMenu;
+using SNetwork;
+using TheArchive.Core;
+using TheArchive.Utilities;
+using static TheArchive.Core.ArchivePatcher;
+
+namespace TheArchive.HarmonyPatches.Patches
+{
+    [BindPatchToSetting(nameof(ArchiveSettings.EnableQualityOfLifeImprovements), "QOL")]
+    internal class UnreadyButtonPatches
+    {
+        [ArchivePatch(typeof(CM_PageLoadout), "UpdateReadyButton", Utils.RundownFlags.RundownOne, Utils.RundownFlags.RundownFive)]
+        internal static class CM_PageLoadout_UpdateReadyButtonPatch
+        {
+#if IL2CPP
+            public static void Postfix(CM_PageLoadout __instance)
+            {
+                var readyButton = __instance.m_readyButton;
+#else
+            public static void Postfix(CM_TimedButton ___m_readyButton)
+            {
+                var readyButton = ___m_readyButton;
+#endif
+                if (!SNet.IsInLobby)
+                    return;
+
+                if (SNet.IsMaster)
+                    return;
+
+                if (GameStateManager.IsReady)
+                {
+                    readyButton.SetText("UNREADY");
+                    readyButton.SetButtonEnabled(true);
+                }
+            }
+
+        }
+    }
+}
