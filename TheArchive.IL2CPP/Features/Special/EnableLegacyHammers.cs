7c01f58b7f2d79d6903255cf28bfd95382ace351
AuriRex <xaurirexx@gmail.com>
2022-11-25 23:58:27 +0100

    Added new feature: EnableLegacyHammers
    Re-enables the 4 legacy hammers: Maul, Gavel, Mallet, Sledgehammer

---
+++ TheArchive.IL2CPP/Features/Special/EnableLegacyHammers.cs
@@ -0,0 +1,51 @@
+﻿using GameData;
+using System.Collections.Generic;
+using TheArchive.Core.Attributes;
+using TheArchive.Core.FeaturesAPI;
+using TheArchive.Core.Managers;
+using TheArchive.Interfaces;
+using static TheArchive.Utilities.Utils;
+
+namespace TheArchive.Features.Special
+{
+    [RundownConstraint(RundownFlags.RundownSix, RundownFlags.Latest)]
+    public class EnableLegacyHammers : Feature
+    {
+        public override string Name => "Enable old Hammers";
+
+        public override string Group => FeatureGroups.Special;
+
+        public override string Description => "Re-enable the pre-R6 Hammers:\nMaul, Gavel, Sledge and Mallet";
+
+        public override bool RequiresRestart => true;
+
+        public new static IArchiveLogger FeatureLogger { get; set; }
+
+#if IL2CPP
+        public override void OnEnable()
+        {
+            DataBlockManager.RegisterTransformationForDB<PlayerOfflineGearDataBlock>(Transform, -10);
+        }
+
+        public static void Transform(List<PlayerOfflineGearDataBlock> allPlayerOfflineGearDataBlocks)
+        {
+            var blocksToEnable = new List<string>
+            {
+                "Maul",
+                "Gavel",
+                "Mallet",
+                "Sledgehammer"
+            };
+
+            foreach(var block in allPlayerOfflineGearDataBlocks)
+            {
+                if(blocksToEnable.Contains(block.name))
+                {
+                    FeatureLogger.Success($" > Enabling block: ID: {block.persistentID}, Name: '{block.name}'");
+                    block.internalEnabled = true;
+                }
+            }
+        }
+#endif
+    }
+}
