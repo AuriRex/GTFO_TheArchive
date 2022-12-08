15d17f614e7894980f4d02412d2e92f609eab533
AuriRex <xaurirexx@gmail.com>
2022-12-08 23:42:14 +0100

    PresenceFormatter: Added sub-logger & cleanup

--- TheArchive.Core/Utilities/PresenceFormatter.cs
+++ TheArchive.Core/Utilities/PresenceFormatter.cs
@@ -3,37 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using TheArchive.Core.Managers;
+using TheArchive.Interfaces;
+using TheArchive.Loader;
 
 namespace TheArchive.Utilities
 {
     public static class PresenceFormatter
     {
-        private static Dictionary<string, PresenceFormatProvider> _formatters = new Dictionary<string, PresenceFormatProvider>();
+        private static readonly Dictionary<string, PresenceFormatProvider> _formatters = new Dictionary<string, PresenceFormatProvider>();
 
 
-        private static List<Type> _typesToCheckForProviders = new List<Type>();
+        private static readonly List<Type> _typesToCheckForProviders = new List<Type>();
+
+        private static IArchiveLogger _logger;
+        private static IArchiveLogger Logger => _logger ??= LoaderWrapper.CreateArSubLoggerInstance(nameof(PresenceFormatter), ConsoleColor.DarkMagenta);
 
         internal static void Setup()
         {
-            ArchiveLogger.Debug($"[{nameof(PresenceFormatter)}] Setting up ...");
+            Logger.Debug($"Setting up providers ...");
             typeof(PresenceManager).RegisterAllPresenceFormatProviders(false);
 
             foreach(var type in _typesToCheckForProviders)
             {
                 foreach(var prop in type.GetProperties())
                 {
-                    var lidfp = prop.GetCustomAttribute<PresenceFormatProvider>();
-                    if (lidfp != null)
+                    try
                     {
-                        lidfp.PropertyInfo = prop;
-                        RegisterFormatter(lidfp);
+                        var formatProviderAttribute = prop.GetCustomAttribute<PresenceFormatProvider>();
+                        if (formatProviderAttribute != null)
+                        {
+                            formatProviderAttribute.SetPropertyInfo(prop);
+                            RegisterFormatter(formatProviderAttribute);
+                        }
+                    }
+                    catch(Exception ex)
+                    {
+                        Logger.Exception(ex);
                     }
                 }
             }
 
             foreach(var former in _formatters.Where(kvp => kvp.Value is FallbackPresenceFormatProvider && !((FallbackPresenceFormatProvider)kvp.Value).NoNotImplementedWarning))
             {
-                ArchiveLogger.Warning($"[{nameof(PresenceFormatter)}] Identifier \"{former.Key}\" has not been implemented! Using Fallback default values!");
+                Logger.Warning($"Identifier \"{former.Key}\" has not been implemented! Using Fallback default values!");
             }
 
         }
@@ -53,10 +65,8 @@ namespace TheArchive.Utilities
 
         public static void RegisterFormatter(PresenceFormatProvider pfp)
         {
-            if (!(pfp?.PropertyInfo?.GetGetMethod()?.IsStatic ?? false))
-            {
-                throw new ArgumentException("Invalid PropertyInfo, getter must be static and public!");
-            }
+            if (!pfp.IsValid)
+                return;
 
             bool fallbackOverridden = false;
 
@@ -71,12 +81,12 @@ namespace TheArchive.Utilities
                 }
                 else
                 {
-                    throw new ArgumentException($"Duplicate formatter identifier: \"{pfp.Identifier}\" (\"{pfp.PropertyInfo.DeclaringType.FullName}.{pfp.PropertyInfo.Name}\")");
+                    throw new ArgumentException($"Duplicate formatter identifier: \"{pfp.Identifier}\" (\"{pfp.DebugIdentifier}\")");
                 }
             }
 
             _formatters.Add(pfp.Identifier, pfp);
-            ArchiveLogger.Debug($"[{nameof(PresenceFormatter)}]{(fallbackOverridden ? " (Fallback Overridden)" : ((pfp is FallbackPresenceFormatProvider) ? " (Fallback)" : string.Empty))} Registered: \"{pfp.Identifier}\" => {pfp.PropertyInfo.DeclaringType.FullName}.{pfp.PropertyInfo.Name} (ASM:{pfp.PropertyInfo.DeclaringType.Assembly.GetName().Name})");
+            Logger.Debug($"{(fallbackOverridden ? " (Fallback Overridden)" : ((pfp is FallbackPresenceFormatProvider) ? " (Fallback)" : string.Empty))} Registered: \"{pfp.Identifier}\" => {pfp.DebugIdentifier}");
         }
 
         internal static object Get(string identifier)
@@ -85,7 +95,7 @@ namespace TheArchive.Utilities
 
             if (former == null) return null;
 
-            return former.PropertyInfo?.GetValue(null);
+            return former.GetValue();
         }
 
         internal static T Get<T>(string identifier)
@@ -94,9 +104,9 @@ namespace TheArchive.Utilities
 
             if (former == null) return default(T);
 
-            if (!former.PropertyInfo.PropertyType.IsAssignableFrom(typeof(T)) && typeof(T) != typeof(string)) throw new ArgumentException($"The property at identifier \"{identifier}\" is not declared as Type \"{typeof(T).Name}\"!");
+            if (!former.PropertyType.IsAssignableFrom(typeof(T)) && typeof(T) != typeof(string)) throw new ArgumentException($"The property at identifier \"{identifier}\" is not declared as Type \"{typeof(T).Name}\"!");
 
-            return (T) former.PropertyInfo?.GetValue(null);
+            return (T) former.GetValue();
         }
 
         public static string Format(this string formatString, params (string search, string replace)[] extraFormatters) => FormatPresenceString(formatString, extraFormatters);
@@ -112,7 +122,7 @@ namespace TheArchive.Utilities
             foreach(var former in _formatters)
             {
                 if(formatted.Contains($"%{former.Key}%"))
-                    formatted = formatted.ReplaceCaseInsensitive($"%{former.Key}%", former.Value.PropertyInfo.GetValue(null)?.ToString() ?? "null");
+                    formatted = formatted.ReplaceCaseInsensitive($"%{former.Key}%", former.Value.GetValue()?.ToString() ?? "null");
             }
 
             if(extraFormatters != null)
@@ -135,11 +145,31 @@ namespace TheArchive.Utilities
         {
             public string Identifier { get; private set; }
 
-            public PropertyInfo PropertyInfo { get; internal set; }
+            public bool IsValid => _propertyInfo != null;
+            public Type PropertyType => PropertyInfo.PropertyType;
+            public string DebugIdentifier => $"{PropertyInfo.DeclaringType.Name}.{PropertyInfo.Name} (ASM:{PropertyInfo.DeclaringType.Assembly.GetName().Name})";
+
+            private PropertyInfo _propertyInfo;
+            internal PropertyInfo PropertyInfo => _propertyInfo ?? throw new Exception($"PropertyInfo not set on {nameof(PresenceFormatProvider)} with ID \"{Identifier}\"!");
 
             public PresenceFormatProvider(string identifier)
             {
                 Identifier = identifier;
+            }
+
+            internal void SetPropertyInfo(PropertyInfo propertyInfo)
+            {
+                if (propertyInfo == null)
+                    throw new ArgumentException($"Provided {nameof(PresenceFormatProvider)} {nameof(PropertyInfo)} may not be null! (ID:{Identifier})");
+                if (!propertyInfo.GetGetMethod()?.IsStatic ?? true)
+                    throw new ArgumentException($"Provided {nameof(PresenceFormatProvider)} {nameof(PropertyInfo)} has to implement a static get method! (ID:{Identifier})");
+
+                _propertyInfo = propertyInfo;
+            }
+
+            public object GetValue()
+            {
+                return PropertyInfo.GetValue(null);
             }
         }
 
