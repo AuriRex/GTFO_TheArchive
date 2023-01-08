6c93a4b66c7c1971ee1ccd4a1a86a0b6d1017f10
AuriRex <xaurirexx@gmail.com>
2023-01-08 01:22:04 +0100

    wtf.

---
+++ TheArchive.MONO/Features/Dev/WTF.cs
@@ -0,0 +1,30 @@
+﻿using TheArchive.Core.Attributes;
+using TheArchive.Core.FeaturesAPI;
+using TheArchive.Interfaces;
+
+namespace TheArchive.Features.Dev
+{
+    [HideInModSettings]
+    [DoNotSaveToConfig]
+    [EnableFeatureByDefault]
+    public class WTF : Feature
+    {
+        public override string Name => "GlobalThreadTerminator3400 Terminator";
+
+        public override string Group => FeatureGroups.Dev;
+
+        public override string Description => "This seems to be the little sh*thead who was preventing my code from saving my config files in R1! >:(";
+
+        public new static IArchiveLogger FeatureLogger { get; set; }
+
+        [ArchivePatch(typeof(GlobalThreadTerminator3400), "OnApplicationQuit")]
+        internal static class GlobalThreadTerminator3400_OnApplicationQuit_Patch
+        {
+            public static bool Prefix()
+            {
+                FeatureLogger.Notice("Terminated GlobalThreadTerminator3400!!!");
+                return ArchivePatch.SKIP_OG;
+            }
+        }
+    }
+}
