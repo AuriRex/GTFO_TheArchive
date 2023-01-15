a1ec27fec9493f6b6582ca4d751f90427b64ba16
AuriRex <xaurirexx@gmail.com>
2023-01-15 19:02:40 +0100

    DVO: Added custom FocusChanged Bahviour Fixed volume resetting on focus change during drop/intro

--- TheArchive.IL2CPP/Features/Accessibility/DynamicVolumeOverride.cs
+++ TheArchive.IL2CPP/Features/Accessibility/DynamicVolumeOverride.cs
@@ -1,8 +1,11 @@
 ﻿using CellMenu;
+using System;
 using System.Collections;
 using TheArchive.Core.Attributes;
 using TheArchive.Core.Attributes.Feature.Settings;
 using TheArchive.Core.FeaturesAPI;
+using TheArchive.Core.FeaturesAPI.Components;
+using TheArchive.Core.FeaturesAPI.Settings;
 using TheArchive.Interfaces;
 using TheArchive.Utilities;
 using UnityEngine;
@@ -20,6 +23,8 @@ namespace TheArchive.Features.Accessibility
 
         public override bool PlaceSettingsInSubMenu => true;
 
+        public static bool IsOverrideActive { get; private set; } = false;
+
         public new static IArchiveLogger FeatureLogger { get; set; }
 
         [FeatureConfig]
@@ -35,6 +40,73 @@ namespace TheArchive.Features.Accessibility
             [FSDisplayName("Volume Interpolation (Seconds)")]
             [FSDescription("The time it takes to transition from the override volume to the game settings volume.\n(Interpolation time)")]
             public float LerpTime { get; set; } = 5f;
+
+            [FSDisplayName("Focus Change Behaviour")]
+            [FSDescription($"Set what happens to audio whenever you tab out.\n\n<color=orange>{nameof(FocusChangeMode.Default)}</color> Use the games implementation\n<color=orange>{nameof(FocusChangeMode.MuteInBackground)}</color> Mute audio when not focused\n<color=orange>{nameof(FocusChangeMode.DoNothingInBackground)}</color> Keep audio on unfocus\n<color=orange>{nameof(FocusChangeMode.OverrideInBackground)}</color> Use override volume on unfocus")]
+            [FSIdentifier(nameof(FocusChangeBehaviour))]
+            public FocusChangeMode FocusChangeBehaviour { get; set; } = FocusChangeMode.OverrideInBackground;
+
+            [FSDisplayName("Reset Game Volume")]
+            [FSDescription("Press if for some reason the audio volume settings don't seem to be back at their default values even though they should be.")]
+            public FButton OopsMySoundSomehowBrokePleaseReset => new FButton("Reset Volume");
+
+            public enum FocusChangeMode
+            {
+                Default,
+                MuteInBackground,
+                DoNothingInBackground,
+                OverrideInBackground
+            }
+        }
+
+        public override void OnButtonPressed(ButtonSetting setting)
+        {
+            if(setting.ButtonID == nameof(DynamicVolumeSettings.OopsMySoundSomehowBrokePleaseReset))
+            {
+                ResetAllVolume();
+            }
+        }
+
+        public override void OnFeatureSettingChanged(FeatureSetting setting)
+        {
+            if(setting.Identifier == nameof(DynamicVolumeSettings.FocusChangeBehaviour))
+            {
+                ResetAllVolume();
+            }
+        }
+
+        public override void OnApplicationFocusChanged(bool focus)
+        {
+            if (Settings.FocusChangeBehaviour == DynamicVolumeSettings.FocusChangeMode.Default)
+                return;
+
+            if (focus)
+            {
+                if (IsOverrideActive)
+                {
+                    SetSFXVolume(Settings.VolumeOverride);
+                    return;
+                }
+                ResetAllVolume();
+            }
+            else
+            {
+                switch(Settings.FocusChangeBehaviour)
+                {
+                    case DynamicVolumeSettings.FocusChangeMode.Default:
+                        break;
+                    case DynamicVolumeSettings.FocusChangeMode.MuteInBackground:
+                        SetAllVolume(0f);
+                        break;
+                    case DynamicVolumeSettings.FocusChangeMode.DoNothingInBackground:
+                        ResetAllVolume();
+                        break;
+                    case DynamicVolumeSettings.FocusChangeMode.OverrideInBackground:
+                        SetAllVolume(Settings.VolumeOverride);
+                        break;
+                }
+                return;
+            }
         }
 
         public static float GetPlayerSFXSettings()
@@ -53,6 +125,24 @@ namespace TheArchive.Features.Accessibility
             CellSound.SetGlobalRTPCValue(AK.GAME_PARAMETERS.VOLUME_SETTING_SFX, volume);
         }
 
+        /// <summary>
+        /// 0 to 100
+        /// </summary>
+        /// <param name="volume"></param>
+        public static void SetAllVolume(float volume)
+        {
+            CellSound.SetGlobalRTPCValue(AK.GAME_PARAMETERS.VOLUME_SETTING_SFX, volume);
+            CellSound.SetGlobalRTPCValue(AK.GAME_PARAMETERS.VOLUME_SETTING_MUSIC, volume);
+            CellSound.SetGlobalRTPCValue(AK.GAME_PARAMETERS.VOLUME_SETTING_DIALOG, volume);
+        }
+
+        public static void ResetAllVolume()
+        {
+            CellSound.SetGlobalRTPCValue(AK.GAME_PARAMETERS.VOLUME_SETTING_SFX, CellSettingsManager.SettingsData.Audio.SFXVolume.Value * 100f);
+            CellSound.SetGlobalRTPCValue(AK.GAME_PARAMETERS.VOLUME_SETTING_MUSIC, CellSettingsManager.SettingsData.Audio.MusicVolume.Value * 100f);
+            CellSound.SetGlobalRTPCValue(AK.GAME_PARAMETERS.VOLUME_SETTING_DIALOG, CellSettingsManager.SettingsData.Audio.DialogVolume.Value * 100f);
+        }
+
         private static eGameStateName _eGameStateName_Offline = Utils.GetEnumFromName<eGameStateName>(nameof(eGameStateName.Offline));
         private static eGameStateName _eGameStateName_StopElevatorRide = Utils.GetEnumFromName<eGameStateName>(nameof(eGameStateName.StopElevatorRide));
 
@@ -60,6 +150,7 @@ namespace TheArchive.Features.Accessibility
         {
             if(state == _eGameStateName_Offline)
             {
+                IsOverrideActive = true;
                 // Change volume for intro seq.
                 SetSFXVolume(Settings.VolumeOverride, "OnGameStateChanged, Offline");
             }
@@ -67,16 +158,19 @@ namespace TheArchive.Features.Accessibility
             if(state == _eGameStateName_StopElevatorRide)
             {
                 // Lerp to game settings on elevator ride stop
-                LerpVolume(Settings.VolumeOverride, GetPlayerSFXSettings(), Settings.LerpTime);
+                LerpVolume(Settings.VolumeOverride, GetPlayerSFXSettings(), Settings.LerpTime, () =>
+                {
+                    IsOverrideActive = false;
+                });
             }
         }
 
-        private static void LerpVolume(float from, float to, float duration)
+        private static void LerpVolume(float from, float to, float duration, Action onDone = null)
         {
-            Loader.LoaderWrapper.StartCoroutine(LerpVolumeCoroutine(from, to, duration));
+            Loader.LoaderWrapper.StartCoroutine(LerpVolumeCoroutine(from, to, duration, onDone));
         }
 
-        private static IEnumerator LerpVolumeCoroutine(float from, float to, float duration)
+        private static IEnumerator LerpVolumeCoroutine(float from, float to, float duration, Action onDone = null)
         {
             if(duration <= 0)
             {
@@ -94,7 +188,9 @@ namespace TheArchive.Features.Accessibility
 
                 var volume = Mathf.Lerp(from, to, factor);
 
-                SetSFXVolume(volume);
+                if(Settings.FocusChangeBehaviour != DynamicVolumeSettings.FocusChangeMode.MuteInBackground
+                    || IsApplicationFocused)
+                    SetSFXVolume(volume);
 
                 timePassed += Mathf.Min(Time.deltaTime, duration - timePassed);
 
@@ -102,6 +198,7 @@ namespace TheArchive.Features.Accessibility
             }
 
             SetSFXVolume(to, "Lerp End");
+            onDone?.Invoke();
             yield break;
         }
 
@@ -116,8 +213,12 @@ namespace TheArchive.Features.Accessibility
                     // Gets called once early when the gameobject is created.
                     // And a second time later once enabled for real (also everytime you switch in the menu)
                     if (_state == 1)
-                        LerpVolume(Settings.VolumeOverride, GetPlayerSFXSettings(), Settings.LerpTime);
-                        //SetSFXVolume(GetPlayerSFXSettings(), "CM_PageRundown_New.OnEnable, _state = 1");
+                    {
+                        LerpVolume(Settings.VolumeOverride, GetPlayerSFXSettings(), Settings.LerpTime, () =>
+                        {
+                            IsOverrideActive = false;
+                        });
+                    }
 
                     _state++;
                 }
@@ -129,7 +230,22 @@ namespace TheArchive.Features.Accessibility
         {
             public static void Prefix()
             {
+                IsOverrideActive = true;
                 SetSFXVolume(Settings.VolumeOverride, "ElevatorRide.StartPreReleaseSequence");
+            }
+        }
+
+        //private void OnApplicationFocus(bool focus)
+        [RundownConstraint(Utils.RundownFlags.RundownTwo, Utils.RundownFlags.Latest)]
+        [ArchivePatch(typeof(CellSettingsManager), UnityMessages.OnApplicationFocus)]
+        internal static class CellSettingsManager_OnApplicationFocus_Patch
+        {
+            public static bool Prefix(bool focus)
+            {
+                if (Settings.FocusChangeBehaviour == DynamicVolumeSettings.FocusChangeMode.Default)
+                    return ArchivePatch.RUN_OG;
+
+                return ArchivePatch.SKIP_OG;
             }
         }
     }
