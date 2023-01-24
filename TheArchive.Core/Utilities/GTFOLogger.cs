3858d42be4ed5678e0ac68c4d1e63486bf82bc01
AuriRex <xaurirexx@gmail.com>
2023-01-24 23:47:11 +0100

    Reduce log spam from weapon switching.

--- TheArchive.Core/Utilities/GTFOLogger.cs
+++ TheArchive.Core/Utilities/GTFOLogger.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using TheArchive.Interfaces;
 
 namespace TheArchive.Utilities
@@ -7,28 +8,35 @@ namespace TheArchive.Utilities
     {
         internal static IArchiveLogger Logger { private get; set; }
 
-        private static readonly HashSet<string> _ignoreList = new HashSet<string>() {
+        private static readonly HashSet<string> _ignoreListExact = new HashSet<string>() {
             "show crosshair",
-            "Setting and getting Body Position/Rotation, IK Goals, Lookat and BoneLocalRotation should only be done in OnAnimatorIK or OnStateIK"
+            "Setting and getting Body Position/Rotation, IK Goals, Lookat and BoneLocalRotation should only be done in OnAnimatorIK or OnStateIK",
         };
 
-        public static void Ignore(string str) => _ignoreList.Add(str);
+        private static readonly HashSet<string> _ignoreListStartsWith = new HashSet<string>() {
+            "Wielding new item in slot",
+        };
+
+        public static void Ignore(string str) => _ignoreListExact.Add(str);
 
         public static void Log(string message)
         {
-            if (_ignoreList.Contains(message)) return;
+            if (_ignoreListExact.Contains(message)) return;
+            if (_ignoreListStartsWith.Any(s => message.StartsWith(s))) return;
             Logger.Info(message);
         }
 
         public static void Warn(string message)
         {
-            if (_ignoreList.Contains(message)) return;
+            if (_ignoreListExact.Contains(message)) return;
+            if (_ignoreListStartsWith.Any(s => message.StartsWith(s))) return;
             Logger.Warning(message);
         }
 
         public static void Error(string message)
         {
-            if (_ignoreList.Contains(message)) return;
+            if (_ignoreListExact.Contains(message)) return;
+            if (_ignoreListStartsWith.Any(s => message.StartsWith(s))) return;
             Logger.Error(message);
         }
     }
