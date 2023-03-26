2481de2a3dd30fac7febc6f140a0dd8068ce34b7
AuriRex <xaurirexx@gmail.com>
2023-03-26 22:04:01 +0200

    Added Feature: "NoDeadPings"

---
+++ TheArchive.IL2CPP/Features/QoL/NoDeadPings.cs
@@ -0,0 +1,45 @@
+﻿using Enemies;
+using TheArchive.Core.Attributes;
+using TheArchive.Core.FeaturesAPI;
+using TheArchive.Utilities;
+
+namespace TheArchive.Features.QoL
+{
+    [EnableFeatureByDefault]
+    public class NoDeadPings : Feature
+    {
+        public override string Name => "No Dead Pings";
+
+        public override string Group => FeatureGroups.QualityOfLife;
+
+        public override string Description => "Prevents bio pings (<#F00><u>/\\</u></color>) on dead enemies.";
+
+
+        [ArchivePatch(typeof(EnemyAgent), nameof(EnemyAgent.SyncPlaceNavMarkerTag))]
+        internal static class EnemyAgent_SyncPlaceNavMarkerTag_Patch
+        {
+            private static IValueAccessor<EnemyAgent, bool> _A_m_alive;
+
+            public static void Init()
+            {
+                _A_m_alive = AccessorBase.GetValueAccessor<EnemyAgent, bool>("m_alive");
+            }
+
+            public static bool Prefix(EnemyAgent __instance)
+            {
+                if(!_A_m_alive.Get(__instance))
+                {
+                    return ArchivePatch.SKIP_OG;
+                }
+
+                if(!Is.R6OrLater)
+                {
+                    // R6+ calls RemoveNavMarker before placing again
+                    __instance.RemoveNavMarker();
+                }
+
+                return ArchivePatch.RUN_OG;
+            }
+        }
+    }
+}
