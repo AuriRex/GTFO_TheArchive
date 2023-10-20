27edf681bd5ae5516d4abff1b019ad3810588b12
AuriRex <xaurirexx@gmail.com>
2023-10-20 14:30:06 +0200

    IS: cleanup & attempted menu audio glitch fix

--- TheArchive.IL2CPP/Features/QoL/IntroSkip.cs
+++ TheArchive.IL2CPP/Features/QoL/IntroSkip.cs
@@ -17,24 +17,51 @@ namespace TheArchive.Features.QoL
 
         public new static IArchiveLogger FeatureLogger { get; set; }
 
-        [ArchivePatch(typeof(CM_PageIntro), "Update")]
-        internal static class CM_PageIntro_UpdatePatch
-        {
-#if MONO
-            private static PropertyAccessor<RundownManager, bool> A_RundownManager_RundownProgressionReady;
 
-            [RundownConstraint(Utils.RundownFlags.RundownFour)]
-            public static void Init()
-            {
+#if MONO
+        private static PropertyAccessor<RundownManager, bool> A_RundownManager_RundownProgressionReady;
+
+        public override void Init()
+        {
+            if (Is.R4OrLater)
                 A_RundownManager_RundownProgressionReady = PropertyAccessor<RundownManager, bool>.GetAccessor("RundownProgressionReady");
-            }
+        }
 #endif
 
+        public static bool IsProgressionFileReady()
+        {
+#if MONO
+            if(Is.R4OrLater)
+                return A_RundownManager_RundownProgressionReady.Get(null);
+            return R3BelowIsProgressionFileReady();
+#else
+            return RundownManager.RundownProgressionReady;
+#endif
+        }
+
+#if MONO
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private static bool R3BelowIsProgressionFileReady()
+        {
+            return RundownManager.PlayerRundownProgressionFileReady;
+        }
+#endif
+
+        [ArchivePatch(typeof(CM_PageIntro), UnityMessages.Update)]
+        internal static class CM_PageIntro_Update_Patch
+        {
             private static bool _injectPressed = false;
+
+            private static MethodAccessor<CM_PageIntro> _onSkip;
+
+            public static void Init()
+            {
+                _onSkip = MethodAccessor<CM_PageIntro>.GetAccessor("OnSkip");
+            }
 
 #if IL2CPP
             [IsPostfix, RundownConstraint(Utils.RundownFlags.RundownFour, Utils.RundownFlags.Latest)]
-            public static void Postfix(CM_PageIntro __instance)
+            public static void PostfixR4OrLater(CM_PageIntro __instance)
             {
                 CheckAndSkipIfReady(__instance, __instance.m_startupLoaded, __instance.m_enemiesLoaded, __instance.m_sharedLoaded);
             }
@@ -53,29 +80,11 @@ namespace TheArchive.Features.QoL
                     && !_injectPressed)
                 {
                     FeatureLogger.Notice("Automatically pressing the Inject button ...");
+                    _onSkip.Invoke(__instance);
                     __instance.EXT_PressInject(-1);
                     _injectPressed = true;
                 }
             }
-
-            public static bool IsProgressionFileReady()
-            {
-#if MONO
-                if(BuildInfo.Rundown.IsIncludedIn(Utils.RundownFlags.RundownFour))
-                    return A_RundownManager_RundownProgressionReady.Get(null);
-                return R3BelowIsProgressionFileReady();
-#else
-                return RundownManager.RundownProgressionReady;
-#endif
-            }
-
-#if MONO
-            [MethodImpl(MethodImplOptions.NoInlining)]
-            private static bool R3BelowIsProgressionFileReady()
-            {
-                return RundownManager.PlayerRundownProgressionFileReady;
-            }
-#endif
         }
     }
 }
