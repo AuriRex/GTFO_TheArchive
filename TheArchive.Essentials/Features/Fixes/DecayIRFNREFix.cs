c9e912985afd8dd62ef31a98aaa8ba6ffee9b997
AuriRex <xaurirexx@gmail.com>
2025-05-22 13:26:23 +0200

    Added Feature 'DecayIRFNREFix'

---
+++ TheArchive.Essentials/Features/Fixes/DecayIRFNREFix.cs
@@ -0,0 +1,41 @@
+using Enemies;
+using GameData;
+using IRF;
+using TheArchive.Core.Attributes;
+using TheArchive.Core.FeaturesAPI;
+using TheArchive.Interfaces;
+using UnityEngine;
+
+namespace TheArchive.Features.Fixes;
+
+[EnableFeatureByDefault]
+public class DecayIRFNREFix : Feature
+{
+    public override string Name => "Decay IRF NRE Fix";
+
+    public override FeatureGroup Group => FeatureGroups.Fixes;
+
+    public override string Description => "Fixes enemies with invalid IRFs spamming the console on death.\n\n<i>Specifically 'tank_boss'</i>";
+    
+    public new static IArchiveLogger FeatureLogger { get; set; }
+    
+    [ArchivePatch(typeof(EnemyPrefabManager), nameof(EnemyPrefabManager.BuildEnemyPrefab))]
+    public static class EnemyPrefabManager__BuildEnemyPrefab__Patch
+    {
+        public static void Postfix(EnemyDataBlock data, ref GameObject __result)
+        {
+            var generatedEnemyPrefab = __result;
+        
+            var irfs = generatedEnemyPrefab.GetComponentsInChildren<InstancedRenderFeature>(includeInactive: true);
+
+            foreach (var irf in irfs)
+            {
+                if (irf.Descriptor != null)
+                    continue;
+            
+                FeatureLogger.Debug($"Found invalid IRF on enemy '{data.name}' (pID: {data.persistentID}), removing ...");
+                UnityEngine.Object.Destroy(irf);
+            }
+        }
+    }
+}
